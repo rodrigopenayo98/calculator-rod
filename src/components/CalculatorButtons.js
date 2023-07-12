f322f3a447ecdb8a6ce38ea90d186f4b94d961a7
rod <91923422+rodrigopenayo98@users.noreply.github.com>
2023-07-12 00:52:02 -0300

    add secundary element

---
+++ src/components/CalculatorButtons.js
@@ -0,0 +1,107 @@
+import React from 'react';
+
+function CalculatorButtons() {
+  return (
+    <div id="buttons-container">
+      <ul id="buttons-list">
+        <li>
+          <button type="button" className="white-button">
+            AC
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            +/-
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            %
+          </button>
+        </li>
+        <li>
+          <button type="button" className="lightblue-button">
+            ÷
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            7
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            8
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            9
+          </button>
+        </li>
+        <li>
+          <button type="button" className="lightblue-button">
+            x
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            4
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            5
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            6
+          </button>
+        </li>
+        <li>
+          <button type="button" className="lightblue-button">
+            -
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            1
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            2
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            3
+          </button>
+        </li>
+        <li>
+          <button type="button" className="lightblue-button">
+            +
+          </button>
+        </li>
+        <li className="button-cero">
+          <button type="button" className="white-button">
+            0
+          </button>
+        </li>
+        <li>
+          <button type="button" className="white-button">
+            .
+          </button>
+        </li>
+        <li>
+          <button type="button" className="lightblue-button">
+            =
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default CalculatorButtons;
